7532327af1c48fea7c49b22aafe4d383bf8c8616
Camilo Martínez <56208148+Kmilo24@users.noreply.github.com>
2024-05-07 12:34:22 -0500

    Final 1.1V

--- screens/CabeCompo.js
+++ screens/CabeCompo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Image, Text, Dimensions, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons'; 
 
 const windowHeight = Dimensions.get('window').height;
 const rectangleHeight = windowHeight / 2;
@@ -13,36 +14,55 @@ const CabeCompo = () => {
     };
 
     return (
-        <TouchableOpacity onPress={goToAnotherScreen}>
-            <View style={{
-                width: Dimensions.get('window').width,
-                height: Dimensions.get('window').height / 7,
-                backgroundColor: '#3B6D7B',
-                position: 'absolute',
-                top: 0,
-                right: 0,
-            }}>
-                <Image
-                    source={require('../assets/logo.png')}
-                    style={{
-                        width: 50,
-                        height: 50,
+        <View>
+            <TouchableOpacity onPress={goToAnotherScreen}>
+                <View style={{
+                    width: Dimensions.get('window').width,
+                    height: Dimensions.get('window').height / 7,
+                    backgroundColor: '#3B6D7B',
+                    position: 'absolute',
+                    top: 0,
+                    right: 0,
+                }}>
+                    <Image
+                        source={require('../assets/logo.png')}
+                        style={{
+                            width: 50,
+                            height: 50,
+                            position: 'absolute',
+                            left: 0,
+                            marginTop: 65,
+                            marginLeft: 20,
+                        }}
+                    />
+                    <Text style={{
+                        color: '#ffffff',
+                        fontSize: 20,
+                        marginTop: 75,
+                        marginLeft: 80,
                         position: 'absolute',
                         left: 0,
-                        marginTop: 65,
-                        marginLeft: 20,
-                    }}
-                />
-                <Text style={{
-                    color: '#ffffff',
-                    fontSize: 20,
-                    marginTop: 75,
-                    marginLeft: 80,
-                    position: 'absolute',
-                    left: 0,
-                }}>Raices Rurales</Text>
-            </View>
-        </TouchableOpacity>
+                    }}>Raices Rurales</Text>
+                    
+                    
+                </View>
+                
+            </TouchableOpacity>
+            <Ionicons name="log-out" size={24} color="white" style={{ color: '#ffffff',
+                        
+                        marginTop: 75,
+                        marginLeft: 250,
+                        position: 'absolute',
+                        left: 0,}}onPress={
+                            () => {
+                                navigation.navigate('Login');
+                            }
+                        }/>
+
+        </View>
+        
+        
+        
     );
 };
 
